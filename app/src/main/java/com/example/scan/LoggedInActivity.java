aebaa229825890114dba5e20013d1fabcc3656a6
adityakaushik27 <adityakaushik27@gmail.com>
2020-05-17 23:27:54 +0530

    Scanner bugs removed

--- app/src/main/java/com/example/scan/LoggedInActivity.java
+++ app/src/main/java/com/example/scan/LoggedInActivity.java
@@ -1,8 +1,12 @@
 package com.example.scan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -39,13 +43,53 @@ public class LoggedInActivity extends AppCompatActivity {
             public void onClick(View view) {
                 ASyncT startHotspot = new ASyncT();
                 startHotspot.execute();
-                startActivity(new Intent(getApplicationContext(), ScannerActivity.class));
-        }});
+//                startActivity(new Intent(getApplicationContext(), ScannerActivity.class));
+                System.out.println("I am here yooo!");
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) !=
+                        PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(com.example.scan.LoggedInActivity.this,
+                            new String[] {Manifest.permission.CAMERA},
+                            50);
+                }
+                else
+                    startActivity(new Intent(getApplicationContext(), ScannerActivity.class));
+            }
+        });
+
     }
 
 
+    ///////////////////////////////////////////
     @Override
-    public void onBackPressed(){        // todo: Check working after adding multiple activites to the activity stack
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 50: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    startActivity(new Intent(getApplicationContext(), ScannerActivity.class));
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    System.out.println("Permission denied by the user!!");
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+    //////////////////////////////////////////
+
+
+
+
+    @Override
+    public void onBackPressed(){        // todo: Check working after adding multiple activities to the activity stack
         if (doublePressToExit){
             Intent intent = new Intent(Intent.ACTION_MAIN);
             intent.addCategory(Intent.CATEGORY_HOME);
