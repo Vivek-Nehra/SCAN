1291ecf44da0308c9969875829050456295bf126
Harshita <harshita01398@gmail.com>
2020-05-28 01:49:41 +0530

    dashboard
    Signed-off-by: Harshita <harshita01398@gmail.com>

--- app/src/main/java/com/example/scan/LoggedInActivity.java
+++ app/src/main/java/com/example/scan/LoggedInActivity.java
@@ -15,15 +15,8 @@ import android.view.View;
 import android.widget.Toast;
 
 
-import com.example.scan.util.User;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class LoggedInActivity extends AppCompatActivity{
 
@@ -54,7 +47,7 @@ public class LoggedInActivity extends AppCompatActivity{
         });
 
 
-        (findViewById(R.id.floatingActionButton)).setOnClickListener(new View.OnClickListener() {
+        (findViewById(R.id.scanButton)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {        // todo : Add all permissions at app startup
                 if (bikeIP == null) {
