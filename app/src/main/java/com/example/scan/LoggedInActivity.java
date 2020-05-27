416da63b6534fc75220ab823c4dfaba2dfdcbacb
Harshita <harshita01398@gmail.com>
2020-05-28 05:10:59 +0530

    user profile code
    Signed-off-by: Harshita <harshita01398@gmail.com>

--- app/src/main/java/com/example/scan/LoggedInActivity.java
+++ app/src/main/java/com/example/scan/LoggedInActivity.java
@@ -6,17 +6,34 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class LoggedInActivity extends AppCompatActivity{
 
@@ -24,7 +41,10 @@ public class LoggedInActivity extends AppCompatActivity{
     private Toast toast = null;
     private String username = null;
     private String bikeIP = null;
-    private int totalDistance = 0;
+    private Uri filePath;
+    private final int PICK_IMAGE_REQUEST = 71;
+    private StorageReference storageReference;
+
 
 
 
@@ -34,6 +54,15 @@ public class LoggedInActivity extends AppCompatActivity{
         setContentView(R.layout.activity_logged_in);
 
         toast = Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT);
+
+        (findViewById(R.id.profile_image)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseImage();
+            }
+
+        });
+
 
 
         (findViewById(R.id.signOut)).setOnClickListener(new View.OnClickListener() {
@@ -109,6 +138,77 @@ public class LoggedInActivity extends AppCompatActivity{
     public void onDestroy(){
         super.onDestroy();
         toast.cancel();
+    }
+
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        Log.d("upload","Here in chooseImage method ");
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+        Log.d("upload","Here in chooseImage method ");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d("upload","Here in activity "+ resultCode);
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("upload","Here in activity "+ resultCode);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            Log.d("upload","Here in try");
+            storageReference = FirebaseStorage.getInstance().getReference();
+            uploadImage();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                de.hdodenhof.circleimageview.CircleImageView profileImage = findViewById(R.id.profile_image);
+                profileImage.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadImage() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            StorageReference ref = storageReference.child("images/"+ currentUser.getUid());
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(LoggedInActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(LoggedInActivity.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
     }
 
 }
